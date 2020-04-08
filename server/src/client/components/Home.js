a57a498359c1a805e629a5290919c3073a7f1676
Howard Tai <howardtai86@gmail.com>
2020-04-09 00:09:06 +0800

    :wrench: section 3 server configuration

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Home = () => <div>I'm the home component</div>;
+const Home = () => (
+  <div>
+    <div>I'm the home component</div>
+    <button onClick={() => console.log('Hi There')}>Press me!</button>
+  </div>
+);
 
 export default Home;
