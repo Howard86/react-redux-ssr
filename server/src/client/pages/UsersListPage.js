8365b099dc8bc0f1e3b682a1c85aa52a0799ed3b
Howard Tai <howardtai86@gmail.com>
2020-04-11 22:21:58 +0800

    :sparkles: add Helmet to UsersList

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
-
+import { Helmet } from 'react-helmet';
 class UserList extends Component {
   componentDidMount() {
     this.props.fetchUsers();
@@ -11,9 +11,19 @@ class UserList extends Component {
     return this.props.users.map(user => <li key={user.id}>{user.name}</li>);
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  }
+
   render() {
     return (
       <div>
+        {this.head()}
         Here's a big list of users:
         <ul>{this.renderUsers()}</ul>
       </div>
