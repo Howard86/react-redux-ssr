2c91fc8a8d22c6270a4aec2640d6c37499ba4c95
Howard Tai <howardtai86@gmail.com>
2020-11-03 22:15:52 +0800

    chore: fix breaking changes

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base');
+const { merge } = require('webpack-merge');
 const webpackNodeExternals = require('webpack-node-externals');
+const baseConfig = require('./webpack.base');
 
 const config = {
   target: 'node',
